ede1311de8e7dbc6f9822da8c0c363ee49ef424c
lofquista <loal23gk@student.ju.se>
2024-03-26 21:26:32 +0100

    added buttons nad styling

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ window.addEventListener("load", (event) => {
       oldInputElement[0].remove();
     }
 
-    // Code adapted from https://www.shecodes.io/athena/77658-how-to-get-the-value-of-an-input-in-javascript and https://chat.openai.com/share/1f576f52-c533-4389-a71e-fd24e6f4030f
+    // Next six rows of code are adapted from https://www.shecodes.io/athena/77658-how-to-get-the-value-of-an-input-in-javascript and https://chat.openai.com/share/1f576f52-c533-4389-a71e-fd24e6f4030f
 
     const todoList = document.getElementById("todoList");
 
@@ -23,6 +23,30 @@ window.addEventListener("load", (event) => {
 
         const addTask = document.createElement("li");
         addTask.textContent = inputValue;
+
+        const finishedButton = document.createElement("button");
+        finishedButton.innerHTML = "✓";
+        finishedButton.style.backgroundColor = "rgb(147, 218, 139)";
+        finishedButton.addEventListener("click", function () {
+          addTask.style.textDecoration = "line-through";
+          addTask.style.color = "grey";
+        });
+
+        const deleteButton = document.createElement("button");
+        deleteButton.innerHTML = "✕";
+        deleteButton.style.backgroundColor = "rgb(242, 43, 41)";
+        deleteButton.style.alignSelf = "flex-end";
+        deleteButton.addEventListener("click", function () {
+          const confirmDelete = confirm(
+            "Are you sure you want to delete this task?"
+          );
+          if (confirmDelete) {
+            addTask.remove();
+          }
+        });
+
+        addTask.appendChild(finishedButton);
+        addTask.appendChild(deleteButton);
 
         todoList.appendChild(addTask);
 
