649a01b47636bc44405bcdad77a2b8d528a717e9
lofquista <loal23gk@student.ju.se>
2024-03-26 17:33:25 +0100

    making tasks appear as list

--- main.js
+++ main.js
@@ -12,6 +12,23 @@ window.addEventListener("load", (event) => {
     if (oldInputElement.length > 1) {
       oldInputElement[0].remove();
     }
+
+    // Code adapted from https://www.shecodes.io/athena/77658-how-to-get-the-value-of-an-input-in-javascript and https://chat.openai.com/share/1f576f52-c533-4389-a71e-fd24e6f4030f
+
+    const todoList = document.getElementById("todoList");
+
+    inputElement.addEventListener("keydown", function (event) {
+      if (event.key === "Enter") {
+        const inputValue = inputElement.value;
+
+        const addTask = document.createElement("li");
+        addTask.textContent = inputValue;
+
+        todoList.appendChild(addTask);
+
+        inputElement.value = "";
+      }
+    });
   }
   addButtonElement.addEventListener("click", onClickHandler);
 });
